07ddf33d9a89fbbb8e0176e1db7f6b210477330f
Tobias Wolff <tobias.wolff@campus.tu-berlin.de>
2020-11-02 22:55:53 +0100

    Added process.xml parser, added indexfile = process

---
+++ orchestrierungMinimalBeispiel/indexFileParser.h
@@ -0,0 +1,28 @@
+#ifndef INDEXFILEPARSER_H
+
+#define INDEXFILEPARSER_H
+
+#include <xercesc/dom/DOM.hpp>
+
+#include <string>
+#include <map>
+
+class indexFileParser
+{
+
+private:
+
+	xercesc_3_2::DOMElement* rootElement;
+
+
+	void initXmlParserGetDocumentGetRootElement(std::string fileName);
+
+public:
+
+	indexFileParser(std::string filename);
+
+	std::map<int, std::string> getProcess();
+
+};
+
+#endif
